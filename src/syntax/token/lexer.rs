ecaed80031ae5c2cdfd9b611536ea31db59971d7
Hanmin Kim <hanminkim123@gmail.com>
2024-01-17 00:00:27 -0500

    add more lexer tests

--- src/syntax/token/lexer.rs
+++ src/syntax/token/lexer.rs
@@ -243,4 +243,137 @@ mod tests {
         assert_eq!(lexer.next().unwrap(), Token::Decimal(0.432432));
         assert_eq!(lexer.next().err().unwrap(), TokenError::MultipleDecimals);
     }
+
+    #[test]
+    fn comment_test() {
+        let statement = "fn main() { // return 0; \n return 1; }";
+        let mut lexer = Lexer::new(StringReader::new(statement.to_string()));
+
+        assert_eq!(lexer.next().unwrap(), Token::Fn);
+        assert_eq!(lexer.next().unwrap(), Token::Ident("main".to_string()));
+        assert_eq!(lexer.next().unwrap(), Token::LParen);
+        assert_eq!(lexer.next().unwrap(), Token::RParen);
+        assert_eq!(lexer.next().unwrap(), Token::LBrace);
+        assert_eq!(lexer.next().unwrap(), Token::Return);
+        assert_eq!(lexer.next().unwrap(), Token::Int(1));
+        assert_eq!(lexer.next().unwrap(), Token::Semicolon);
+        assert_eq!(lexer.next().unwrap(), Token::RBrace);
+        assert_eq!(lexer.next().unwrap(), Token::EOF);
+    }
+
+    #[test]
+    fn whitespace_test() {
+        let statement = "fn main()                   {       return 0; }";
+        let mut lexer = Lexer::new(StringReader::new(statement.to_string()));
+
+        assert_eq!(lexer.next().unwrap(), Token::Fn);
+        assert_eq!(lexer.next().unwrap(), Token::Ident("main".to_string()));
+        assert_eq!(lexer.next().unwrap(), Token::LParen);
+        assert_eq!(lexer.next().unwrap(), Token::RParen);
+        assert_eq!(lexer.next().unwrap(), Token::LBrace);
+        assert_eq!(lexer.next().unwrap(), Token::Return);
+        assert_eq!(lexer.next().unwrap(), Token::Int(0));
+        assert_eq!(lexer.next().unwrap(), Token::Semicolon);
+        assert_eq!(lexer.next().unwrap(), Token::RBrace);
+        assert_eq!(lexer.next().unwrap(), Token::EOF);
+    }
+
+    #[test]
+    fn operator_test() {
+        let statement = "fn main() { return 0 + 1 - 2 * 3 / 4 % 5; }";
+        let mut lexer = Lexer::new(StringReader::new(statement.to_string()));
+
+        assert_eq!(lexer.next().unwrap(), Token::Fn);
+        assert_eq!(lexer.next().unwrap(), Token::Ident("main".to_string()));
+        assert_eq!(lexer.next().unwrap(), Token::LParen);
+        assert_eq!(lexer.next().unwrap(), Token::RParen);
+        assert_eq!(lexer.next().unwrap(), Token::LBrace);
+        assert_eq!(lexer.next().unwrap(), Token::Return);
+        assert_eq!(lexer.next().unwrap(), Token::Int(0));
+        assert_eq!(lexer.next().unwrap(), Token::Plus);
+        assert_eq!(lexer.next().unwrap(), Token::Int(1));
+        assert_eq!(lexer.next().unwrap(), Token::Minus);
+        assert_eq!(lexer.next().unwrap(), Token::Int(2));
+        assert_eq!(lexer.next().unwrap(), Token::Star);
+        assert_eq!(lexer.next().unwrap(), Token::Int(3));
+        assert_eq!(lexer.next().unwrap(), Token::Slash);
+        assert_eq!(lexer.next().unwrap(), Token::Int(4));
+        assert_eq!(lexer.next().unwrap(), Token::Percent);
+        assert_eq!(lexer.next().unwrap(), Token::Int(5));
+        assert_eq!(lexer.next().unwrap(), Token::Semicolon);
+        assert_eq!(lexer.next().unwrap(), Token::RBrace);
+        assert_eq!(lexer.next().unwrap(), Token::EOF);
+    }
+
+    #[test]
+    fn singleton_symbol_test() {
+        let statement = "=,;:.(){}[]<>+-*/%!&";
+        let mut lexer = Lexer::new(StringReader::new(statement.to_string()));
+
+        assert_eq!(lexer.next().unwrap(), Token::Assign);
+        assert_eq!(lexer.next().unwrap(), Token::Comma);
+        assert_eq!(lexer.next().unwrap(), Token::Semicolon);
+        assert_eq!(lexer.next().unwrap(), Token::Colon);
+        assert_eq!(lexer.next().unwrap(), Token::Dot);
+        assert_eq!(lexer.next().unwrap(), Token::LParen);
+        assert_eq!(lexer.next().unwrap(), Token::RParen);
+        assert_eq!(lexer.next().unwrap(), Token::LBrace);
+        assert_eq!(lexer.next().unwrap(), Token::RBrace);
+        assert_eq!(lexer.next().unwrap(), Token::LBracket);
+        assert_eq!(lexer.next().unwrap(), Token::RBracket);
+        assert_eq!(lexer.next().unwrap(), Token::LAngle);
+        assert_eq!(lexer.next().unwrap(), Token::RAngle);
+        assert_eq!(lexer.next().unwrap(), Token::Plus);
+        assert_eq!(lexer.next().unwrap(), Token::Minus);
+        assert_eq!(lexer.next().unwrap(), Token::Star);
+        assert_eq!(lexer.next().unwrap(), Token::Slash);
+        assert_eq!(lexer.next().unwrap(), Token::Percent);
+        assert_eq!(lexer.next().unwrap(), Token::Exclamation);
+        assert_eq!(lexer.next().unwrap(), Token::Ampersand);
+    }
+
+    #[test]
+    fn symbol_equals_test() {
+        let statement = "== != <= >= += -= *= /= %=";
+        let mut lexer = Lexer::new(StringReader::new(statement.to_string()));
+
+        assert_eq!(lexer.next().unwrap(), Token::Equal);
+        assert_eq!(lexer.next().unwrap(), Token::NotEqual);
+        assert_eq!(lexer.next().unwrap(), Token::Leq);
+        assert_eq!(lexer.next().unwrap(), Token::Geq);
+        assert_eq!(lexer.next().unwrap(), Token::PlusAssign);
+        assert_eq!(lexer.next().unwrap(), Token::MinusAssign);
+        assert_eq!(lexer.next().unwrap(), Token::StarAssign);
+        assert_eq!(lexer.next().unwrap(), Token::SlashAssign);
+        assert_eq!(lexer.next().unwrap(), Token::PercentAssign);
+    }
+
+    #[test]
+    fn key_word_test() {
+        let statement = "const static void int float double bool string struct impl fn rec inline if else while for return break continue true false";
+        let mut lexer = Lexer::new(StringReader::new(statement.to_string()));
+
+        assert_eq!(lexer.next().unwrap(), Token::Const);
+        assert_eq!(lexer.next().unwrap(), Token::Static);
+        assert_eq!(lexer.next().unwrap(), Token::VoidType);
+        assert_eq!(lexer.next().unwrap(), Token::IntType);
+        assert_eq!(lexer.next().unwrap(), Token::FloatType);
+        assert_eq!(lexer.next().unwrap(), Token::DoubleType);
+        assert_eq!(lexer.next().unwrap(), Token::BoolType);
+        assert_eq!(lexer.next().unwrap(), Token::StringType);
+        assert_eq!(lexer.next().unwrap(), Token::StructType);
+        assert_eq!(lexer.next().unwrap(), Token::Impl);
+        assert_eq!(lexer.next().unwrap(), Token::Fn);
+        assert_eq!(lexer.next().unwrap(), Token::Rec);
+        assert_eq!(lexer.next().unwrap(), Token::Inline);
+        assert_eq!(lexer.next().unwrap(), Token::If);
+        assert_eq!(lexer.next().unwrap(), Token::Else);
+        assert_eq!(lexer.next().unwrap(), Token::While);
+        assert_eq!(lexer.next().unwrap(), Token::For);
+        assert_eq!(lexer.next().unwrap(), Token::Return);
+        assert_eq!(lexer.next().unwrap(), Token::Break);
+        assert_eq!(lexer.next().unwrap(), Token::Continue);
+        assert_eq!(lexer.next().unwrap(), Token::Bool(true));
+        assert_eq!(lexer.next().unwrap(), Token::Bool(false));
+    }
 }
