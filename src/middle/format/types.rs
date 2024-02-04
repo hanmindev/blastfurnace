58c3fa8fb5f51d4d9f7f91aa432f999881549dc6
Hanmin Kim <hanminkim123@gmail.com>
2024-02-04 14:15:40 -0500

    fix(middle/format): fix derive

--- src/middle/format/types.rs
+++ src/middle/format/types.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
 use crate::front::syntax::ast_types::{FnDef, StructDef, VarDecl};
 
-#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub struct GlobalName {
     pub module: String,
     pub name: String,
 }
-#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+#[derive(Debug, PartialEq)]
 pub struct Program {
     pub function_definitions: HashMap<GlobalName, FnDef>,
     pub struct_definitions: HashMap<GlobalName, StructDef>,
