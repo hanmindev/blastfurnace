bbdb964931fbcf149f84fe35d12521eccba845d1
Hanmin Kim <hanminkim123@gmail.com>
2024-02-07 21:42:46 -0500

    lint(all): lint

--- src/front/mergers/program.rs
+++ src/front/mergers/program.rs
@@ -1,12 +1,12 @@
 use crate::front::ast_retriever::retriever::FileRetriever;
 use crate::front::file_system::fs::FileSystem;
 use crate::front::mergers::convert::{convert_fn, global_name_updater};
+use crate::front::mergers::definition_table::DefinitionTable;
 use crate::front::mergers::package::{Package, Packager};
 use crate::middle::format::types::Program;
 use std::collections::{HashMap, HashSet};
 use std::marker::PhantomData;
 use std::rc::Rc;
-use crate::front::mergers::definition_table::DefinitionTable;
 
 pub struct ProgramMerger<R> {
     root_package: String,
@@ -43,50 +43,54 @@ impl<R: FileSystem> ProgramMerger<R> {
             if package_name == self.root_package {
                 for def in &table.merged_module.public_definitions.function_definitions {
                     public_functions.insert(Rc::clone(def.0));
-                    program
-                        .public_functions
-                        .insert(global_name_updater(&def.0));
+                    program.public_functions.insert(global_name_updater(&def.0));
                 }
             }
 
-            def_table.function_definitions.extend(table
-                .merged_module
-                .public_definitions
-                .function_definitions
-                .drain()
-                .chain(
-                    table
-                        .merged_module
-                        .private_definitions
-                        .function_definitions
-                        .drain(),
-                ));
+            def_table.function_definitions.extend(
+                table
+                    .merged_module
+                    .public_definitions
+                    .function_definitions
+                    .drain()
+                    .chain(
+                        table
+                            .merged_module
+                            .private_definitions
+                            .function_definitions
+                            .drain(),
+                    ),
+            );
 
-            def_table.struct_definitions.extend(table
-                .merged_module
-                .public_definitions
-                .struct_definitions
-                .drain()
-                .chain(
-                    table
-                        .merged_module
-                        .private_definitions
-                        .struct_definitions
-                        .drain(),
-                ));
+            def_table.struct_definitions.extend(
+                table
+                    .merged_module
+                    .public_definitions
+                    .struct_definitions
+                    .drain()
+                    .chain(
+                        table
+                            .merged_module
+                            .private_definitions
+                            .struct_definitions
+                            .drain(),
+                    ),
+            );
 
-            def_table.global_var_definitions.extend(table
-                .merged_module
-                .public_definitions
-                .global_var_definitions
-                .drain()
-                .chain(
-                    table
-                        .merged_module
-                        .private_definitions
-                        .global_var_definitions
-                        .drain(),
-                ));
+            def_table.global_var_definitions.extend(
+                table
+                    .merged_module
+                    .public_definitions
+                    .global_var_definitions
+                    .drain()
+                    .chain(
+                        table
+                            .merged_module
+                            .private_definitions
+                            .global_var_definitions
+                            .drain(),
+                    ),
+            );
         }
 
         for public_function in public_functions {
