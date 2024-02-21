949b7aa3d1a93a530386f2b0ed2faf364914f3be
Hanmin Kim <hanminkim123@gmail.com>
2024-02-20 17:16:16 -0800

    feat(module_resolution): use visitor for module resolution

--- src/front/mergers/package/module_resolution/resolvers.rs
+++ src/front/mergers/package/module_resolution/resolvers.rs
@@ -1,15 +1,7 @@
-use crate::front::ast_types::{
-    AtomicExpression, Block, Definition, Else, Expression, ExpressionEnum, If, Module, Reference,
-    ResolvedName, Statement, Use,
-};
+use crate::front::ast_types::visitor::{ASTNodeEnum, Visitable, Visitor};
+use crate::front::ast_types::{AtomicExpression, Definition, ResolvedName};
 use crate::front::mergers::package::module_resolution::module_merger::ModuleMerger;
 use std::rc::Rc;
-
-pub trait Resolvable {
-    fn resolve_module(&mut self, _module_merger: &mut ModuleMerger) -> ResolveResult<()> {
-        Ok(())
-    }
-}
 
 #[derive(Debug, PartialEq)]
 pub enum ResolverError {
@@ -18,166 +10,109 @@ pub enum ResolverError {
 
 pub type ResolveResult<T> = Result<T, ResolverError>;
 
-impl Resolvable for Block {
-    fn resolve_module(&mut self, module_merger: &mut ModuleMerger) -> ResolveResult<()> {
-        while let Some(definition) = self.definitions.pop() {
-            resolve_definition(definition, module_merger, false)?;
-        }
-
-        for statement in &mut self.statements {
-            statement.resolve_module(module_merger)?;
-        }
-        Ok(())
-    }
-}
-
-impl Resolvable for Module {
-    fn resolve_module(&mut self, module_merger: &mut ModuleMerger) -> ResolveResult<()> {
-        while let Some(mut use_) = self.uses.pop() {
-            use_.resolve_module(module_merger)?;
-        }
-
-        while let Some(definition) = self.public_definitions.pop() {
-            resolve_definition(definition, module_merger, true)?;
-        }
-
-        self.block.resolve_module(module_merger)?;
-        Ok(())
-    }
-}
-
-impl Resolvable for Use {
-    fn resolve_module(&mut self, module_merger: &mut ModuleMerger) -> ResolveResult<()> {
-        for element in &mut self.elements {
-            let original_name = element.origin_name.clone();
-            let local_name: Rc<ResolvedName> = element
-                .imported_name
-                .module_resolved
-                .as_ref()
-                .unwrap()
-                .clone();
-
-            let module_name = if self.path[0] == "root" {
-                let mut s = "/".to_string();
-                s.push_str(&self.path[1..].join("/"));
-                s
-            } else {
-                let s = self.path.join("/");
-
-                if !module_merger.can_call(&s) {
-                    return Err(ResolverError::ImportVisibilityError(
-                        s.clone(),
-                        module_merger.get_path().clone(),
-                        "Cannot call module from this path".to_string(),
-                    ));
+impl Visitor<ResolverError> for ModuleMerger {
+    fn apply(&mut self, ast_node: &mut ASTNodeEnum) -> ResolveResult<bool> {
+        match ast_node {
+            ASTNodeEnum::Block(block) => {
+                while let Some(definition) = block.definitions.pop() {
+                    resolve_definition(definition, self, false)?;
                 }
-                s
-            };
 
-            let global_resolved_name =
-                module_merger.create_or_get_global_name(module_name, format!("0_{original_name}"));
+                for statement in &mut block.statements {
+                    statement.visit(self)?;
+                }
+            }
+            ASTNodeEnum::Module(module) => {
+                while let Some(mut use_) = module.uses.pop() {
+                    use_.visit(self)?;
+                }
 
-            module_merger.register_global_name(local_name, Rc::from(global_resolved_name), false);
-            element.imported_name.resolve_module(module_merger)?;
-        }
+                while let Some(definition) = module.public_definitions.pop() {
+                    resolve_definition(definition, self, true)?;
+                }
 
-        Ok(())
-    }
-}
+                module.block.visit(self)?;
+            }
+            ASTNodeEnum::Use(use_) => {
+                for element in &mut use_.elements {
+                    let original_name = element.origin_name.clone();
+                    let local_name: Rc<ResolvedName> = element
+                        .imported_name
+                        .module_resolved
+                        .as_ref()
+                        .unwrap()
+                        .clone();
 
-impl Resolvable for Reference {
-    fn resolve_module(&mut self, module_merger: &mut ModuleMerger) -> ResolveResult<()> {
-        self.global_resolved =
-            Some(module_merger.resolve_global_name(self.module_resolved.as_ref().unwrap()));
+                    let module_name = if use_.path[0] == "root" {
+                        let mut s = "/".to_string();
+                        s.push_str(&use_.path[1..].join("/"));
+                        s
+                    } else {
+                        let s = use_.path.join("/");
 
-        Ok(())
-    }
-}
+                        if !self.can_call(&s) {
+                            return Err(ResolverError::ImportVisibilityError(
+                                s.clone(),
+                                self.get_path().clone(),
+                                "Cannot call module from this path".to_string(),
+                            ));
+                        }
+                        s
+                    };
 
-impl Resolvable for Else {
-    fn resolve_module(&mut self, module_merger: &mut ModuleMerger) -> ResolveResult<()> {
-        match self {
-            Else::If(if_) => if_.resolve_module(module_merger)?,
-            Else::Block(block) => block.resolve_module(module_merger)?,
-        }
-        Ok(())
-    }
-}
+                    let global_resolved_name =
+                        self.create_or_get_global_name(module_name, format!("0_{original_name}"));
 
-impl Resolvable for If {
-    fn resolve_module(&mut self, module_merger: &mut ModuleMerger) -> ResolveResult<()> {
-        self.cond.resolve_module(module_merger)?;
-        self.body.resolve_module(module_merger)?;
-        if let Some(else_block) = &mut self.else_ {
-            else_block.resolve_module(module_merger)?;
-        }
-        Ok(())
-    }
-}
+                    self.register_global_name(local_name, Rc::from(global_resolved_name), false);
+                    element.imported_name.visit(self)?;
+                }
+            }
+            ASTNodeEnum::Reference(reference) => {
+                reference.global_resolved =
+                    Some(self.resolve_global_name(reference.module_resolved.as_ref().unwrap()));
+            }
 
-impl Resolvable for Statement {
-    fn resolve_module(&mut self, module_merger: &mut ModuleMerger) -> ResolveResult<()> {
-        match self {
-            Statement::VarDecl(var_decl) => {
-                var_decl.var_def.name.resolve_module(module_merger)?;
+            ASTNodeEnum::VarDecl(var_decl) => {
                 if let Some(expr) = &mut var_decl.expr {
-                    expr.resolve_module(module_merger)?;
+                    expr.visit(self)?;
                 }
+                var_decl.var_def.name.visit(self)?;
             }
-            Statement::VarAssign(var_assign) => {
-                var_assign.name_path.name.resolve_module(module_merger)?;
-                var_assign.expr.resolve_module(module_merger)?;
-            }
-            Statement::If(if_) => {
-                if_.resolve_module(module_merger)?;
-            }
-            Statement::While(while_) => {
-                while_.cond.resolve_module(module_merger)?;
-                while_.body.resolve_module(module_merger)?;
-            }
-            Statement::For(for_) => {
-                if let Some(init) = &mut for_.init {
-                    init.resolve_module(module_merger)?;
-                }
-                if let Some(cond) = &mut for_.cond {
-                    cond.resolve_module(module_merger)?;
-                }
-                if let Some(step) = &mut for_.step {
-                    step.resolve_module(module_merger)?;
-                }
-                for_.body.resolve_module(module_merger)?;
-            }
-            Statement::Expression(expr) => {
-                expr.resolve_module(module_merger)?;
-            }
-            Statement::Block(block) => block.resolve_module(module_merger)?,
-            _ => {}
-        }
-        Ok(())
-    }
-}
 
-impl Resolvable for Expression {
-    fn resolve_module(&mut self, module_merger: &mut ModuleMerger) -> ResolveResult<()> {
-        match &mut self.expr {
-            ExpressionEnum::AtomicExpression(atomic) => match atomic {
-                AtomicExpression::Literal(_) => {}
-                AtomicExpression::Variable(var) => {
-                    var.name.resolve_module(module_merger)?;
-                }
-                AtomicExpression::FnCall(fn_call) => {
-                    fn_call.name.resolve_module(module_merger)?;
-                }
-            },
-            ExpressionEnum::Unary(_, expr) => {
-                expr.resolve_module(module_merger)?;
+            ASTNodeEnum::VarAssign(var_assign) => {
+                var_assign.expr.visit(self)?;
+                var_assign.name_path.name.visit(self)?;
             }
-            ExpressionEnum::Binary(e0, _, e1) => {
-                e0.resolve_module(module_merger)?;
-                e1.resolve_module(module_merger)?;
+
+            ASTNodeEnum::AtomicExpression(atomic) => {
+                match atomic {
+                    AtomicExpression::Literal(_) => {}
+                    AtomicExpression::Variable(var) => {
+                        var.name.visit(self)?;
+                    }
+                    AtomicExpression::FnCall(fn_call) => {
+                        fn_call.name.visit(self)?;
+                    }
+                }
             }
-        }
-        Ok(())
+
+            ASTNodeEnum::Else(_)
+            | ASTNodeEnum::If(_)
+            | ASTNodeEnum::While(_)
+            | ASTNodeEnum::For(_)
+            | ASTNodeEnum::Statement(_)
+            | ASTNodeEnum::Expression(_) => return Ok(true),
+
+            ASTNodeEnum::FnDef(_)
+            | ASTNodeEnum::StructDef(_)
+            | ASTNodeEnum::Definition(_) => panic!("Should not be called directly"),
+
+            ASTNodeEnum::VarDef(_)
+            | ASTNodeEnum::NamePath(_)
+            | ASTNodeEnum::FnCall(_)
+            | ASTNodeEnum::LiteralValue(_) => return Ok(false),
+        };
+        return Ok(false);
     }
 }
 
@@ -188,8 +123,8 @@ fn resolve_definition(
 ) -> ResolveResult<()> {
     match definition {
         Definition::FnDef(mut fn_def) => {
-            fn_def.name.resolve_module(module_merger)?;
-            fn_def.body.resolve_module(module_merger)?;
+            fn_def.name.visit(module_merger)?;
+            fn_def.body.visit(module_merger)?;
 
             module_merger.insert_fn_definition(
                 fn_def.name.global_resolved.clone().unwrap(),
@@ -198,7 +133,7 @@ fn resolve_definition(
             );
         }
         Definition::StructDef(mut struct_def) => {
-            struct_def.type_name.resolve_module(module_merger)?;
+            struct_def.type_name.visit(module_merger)?;
 
             module_merger.insert_struct_definition(
                 struct_def.type_name.global_resolved.clone().unwrap(),
@@ -207,9 +142,9 @@ fn resolve_definition(
             );
         }
         Definition::VarDecl(mut var_decl) => {
-            var_decl.var_def.name.resolve_module(module_merger)?;
+            var_decl.var_def.name.visit(module_merger)?;
             if let Some(expr) = &mut var_decl.expr {
-                expr.resolve_module(module_merger)?;
+                expr.visit(module_merger)?;
             }
 
             module_merger.insert_global_var_definition(
