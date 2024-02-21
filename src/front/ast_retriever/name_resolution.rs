b39e07671706bf9d43f994f618fdfbddf8966588
Hanmin Kim <hanminkim123@gmail.com>
2024-02-20 16:45:47 -0800

    fix(front/ast_retriever/name_resolution): use visitor for name resolution

--- src/front/ast_retriever/name_resolution.rs
+++ src/front/ast_retriever/name_resolution.rs
@@ -1,11 +1,12 @@
-use crate::front::ast_retriever::name_resolution::resolvers::Resolvable;
 use crate::front::ast_retriever::name_resolution::scope_table::ScopeTable;
 use crate::front::ast_types::Module;
+use crate::front::ast_types::visitor::Visitable;
 
 mod lib;
 mod resolvers;
 mod scope_table;
+
 pub fn resolve_module(module: &mut Module) {
     let mut scope_table = ScopeTable::new();
-    module.resolve_name(&mut scope_table).unwrap();
+    module.visit(&mut scope_table).unwrap();
 }
--- src/front/ast_retriever/name_resolution/resolvers.rs
+++ src/front/ast_retriever/name_resolution/resolvers.rs
@@ -1,11 +1,6 @@
 use crate::front::ast_retriever::name_resolution::scope_table::{ScopeTable, SymbolType};
-use crate::front::ast_types::{AtomicExpression, Block, Compound, CompoundValue, Definition, Else, Expression, ExpressionEnum, FnCall, FnDef, For, If, LiteralValue, Module, NamePath, Statement, StructDef, Type, Use, VarAssign, VarDecl, VarDef, While};
-
-pub trait Resolvable {
-    fn resolve_name(&mut self, _scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        Ok(())
-    }
-}
+use crate::front::ast_types::{AtomicExpression, Block, Compound, CompoundValue, Definition, Else, Expression, ExpressionEnum, FnCall, FnDef, For, If, LiteralValue, Module, NamePath, Reference, Statement, StructDef, Type, Use, VarAssign, VarDecl, VarDef, While};
+use crate::front::ast_types::visitor::{ASTNodeEnum, Visitable, Visitor};
 
 #[derive(Debug, PartialEq)]
 pub enum ResolverError {
@@ -15,270 +10,126 @@ pub enum ResolverError {
 
 pub type ResolveResult<T> = Result<T, ResolverError>;
 
-impl Resolvable for Block {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        scope_table.scope_enter();
-        for definitions in &mut self.definitions {
-            definitions.resolve_name(scope_table)?;
+fn name_path_lookup(scope_table: &ScopeTable, name_path: &mut NamePath) -> ResolveResult<()> {
+    match scope_table.scope_lookup(&name_path.name.raw, SymbolType::Var) {
+        Some(name) => {
+            name_path.name.module_resolved = Some(name.clone());
+            Ok(())
         }
-
-        for statement in &mut self.statements {
-            statement.resolve_name(scope_table)?;
-        }
-        scope_table.scope_exit();
-        Ok(())
+        None => Err(ResolverError::UndefinedVariable(name_path.name.raw.clone())),
     }
 }
 
-impl Resolvable for Module {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        scope_table.scope_enter();
-        for use_ in &mut self.uses {
-            use_.resolve_name(scope_table)?;
-        }
-
-        for definitions in &mut self.public_definitions {
-            definitions.resolve_name(scope_table)?;
-        }
-
-        self.block.resolve_name(scope_table)?;
-        scope_table.scope_exit();
-        Ok(())
-    }
-}
-
-impl Resolvable for Definition {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        match self {
-            Definition::VarDecl(statement) => statement.resolve_name(scope_table)?,
-            Definition::StructDef(statement) => statement.resolve_name(scope_table)?,
-            Definition::FnDef(statement) => statement.resolve_name(scope_table)?,
-        }
-        Ok(())
-    }
-}
-
-impl Resolvable for Statement {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        match self {
-            Statement::VarDecl(statement) => statement.resolve_name(scope_table)?,
-            Statement::VarAssign(statement) => statement.resolve_name(scope_table)?,
-            Statement::If(statement) => statement.resolve_name(scope_table)?,
-            Statement::While(statement) => statement.resolve_name(scope_table)?,
-            Statement::For(statement) => statement.resolve_name(scope_table)?,
-            Statement::Return(statement) => statement.resolve_name(scope_table)?,
-            Statement::Expression(statement) => statement.resolve_name(scope_table)?,
-            Statement::Block(block) => block.resolve_name(scope_table)?,
-
-            _ => {}
-        };
-        Ok(())
-    }
-}
-
-impl Resolvable for VarDef {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        if let Some(Type::Struct(struct_name)) = &mut self.type_ {
-            struct_name.module_resolved =
-                Some(scope_table.scope_lookup_force(&struct_name.raw, SymbolType::Struct));
-        }
-
-        self.name.module_resolved = Some(scope_table.scope_bind(&self.name.raw, SymbolType::Var)?);
-        Ok(())
-    }
-}
-
-impl Resolvable for VarDecl {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        if let Some(ref mut expr) = self.expr {
-            expr.resolve_name(scope_table)?
-        }
-        self.var_def.resolve_name(scope_table)?;
-
-        Ok(())
-    }
-}
-
-impl Resolvable for VarAssign {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        self.name_path.resolve_name(scope_table)?;
-        self.expr.resolve_name(scope_table)?;
-
-        Ok(())
-    }
-}
-
-impl Resolvable for StructDef {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        self.type_name.module_resolved =
-            Some(scope_table.scope_bind(&self.type_name.raw, SymbolType::Struct)?);
-
-        for v in &mut self.map.values_mut() {
-            if let Type::Struct(struct_name) = v {
-                struct_name.module_resolved =
-                    Some(scope_table.scope_lookup_force(&struct_name.raw, SymbolType::Struct));
+impl Visitor<ResolverError> for ScopeTable {
+    fn apply(&mut self, ast_node: &mut ASTNodeEnum) -> ResolveResult<bool> {
+        match ast_node {
+            ASTNodeEnum::NamePath(name_path) => {
+                panic!("NamePath should not be visited directly")
             }
-        }
-        Ok(())
-    }
-}
-
-impl Resolvable for FnDef {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        scope_table.scope_enter();
-
-        self.name.module_resolved = Some(scope_table.scope_bind(&self.name.raw, SymbolType::Fn)?);
-        for arg in &mut self.args {
-            arg.resolve_name(scope_table)?;
-        }
-
-        self.body.resolve_name(scope_table)?;
-        scope_table.scope_exit();
-
-        Ok(())
-    }
-}
-
-impl Resolvable for Expression {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        match &mut self.expr {
-            ExpressionEnum::AtomicExpression(atomic) => {
-                atomic.resolve_name(scope_table)?;
+            ASTNodeEnum::Reference(reference) => {
+                panic!("Reference should not be visited directly")
             }
-            ExpressionEnum::Unary(_, expression) => {
-                expression.resolve_name(scope_table)?;
-            }
-            ExpressionEnum::Binary(e0, _, e1) => {
-                e0.resolve_name(scope_table)?;
-                e1.resolve_name(scope_table)?;
-            }
-        }
-
-        Ok(())
-    }
-}
-
-impl Resolvable for AtomicExpression {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        match self {
-            AtomicExpression::Literal(lit) => {
-                if let LiteralValue::Compound(compound) = lit {
-                    compound.resolve_name(scope_table)?;
+            ASTNodeEnum::AtomicExpression(atomic_expression) => {
+                match atomic_expression {
+                    AtomicExpression::Variable(x) => {
+                        match self.scope_lookup(&x.name.raw, SymbolType::Var) {
+                            Some(name) => {
+                                x.name.module_resolved = Some(name.clone());
+                            }
+                            None => Err(ResolverError::UndefinedVariable(x.name.raw.clone()))?,
+                        }
+                    }
+                    _=> { return Ok(true); }
                 }
             }
-            AtomicExpression::Variable(var) => {
-                var.resolve_name(scope_table)?;
+
+            ASTNodeEnum::VarDef(var_def) => {
+                if let Some(Type::Struct(struct_name)) = &mut var_def.type_ {
+                    struct_name.module_resolved =
+                        Some(self.scope_lookup_force(&struct_name.raw, SymbolType::Struct));
+                }
+
+                var_def.name.module_resolved = Some(self.scope_bind(&var_def.name.raw, SymbolType::Var)?);
             }
-            AtomicExpression::FnCall(fn_call) => {
-                fn_call.resolve_name(scope_table)?;
+            ASTNodeEnum::VarAssign(var_assign) => {
+                var_assign.expr.visit(self)?;
+                name_path_lookup(&self, &mut var_assign.name_path)?;
             }
-        }
-        Ok(())
-    }
-}
+            ASTNodeEnum::FnDef(fn_def) => {
+                self.scope_enter();
 
-impl Resolvable for NamePath {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        match scope_table.scope_lookup(&self.name.raw, SymbolType::Var) {
-            Some(name) => {
-                self.name.module_resolved = Some(name.clone());
-                Ok(())
+                fn_def.name.module_resolved = Some(self.scope_bind(&fn_def.name.raw, SymbolType::Fn)?);
+                for arg in &mut fn_def.args {
+                    arg.visit(self)?;
+                }
+
+                fn_def.body.visit(self)?;
+                self.scope_exit();
             }
-            None => Err(ResolverError::UndefinedVariable(self.name.raw.clone())),
-        }
-    }
-}
+            ASTNodeEnum::FnCall(fn_call) => {
+                match self.scope_lookup(&fn_call.name.raw, SymbolType::Fn) {
+                    Some(name) => {
+                        fn_call.name.module_resolved = Some(name.clone());
+                    }
+                    None => return Err(ResolverError::UndefinedVariable(fn_call.name.raw.clone())),
+                }
 
-impl Resolvable for Compound {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        for (_, value) in self.iter_mut() {
-            match value {
-                CompoundValue::Expression(expr) => expr.resolve_name(scope_table)?,
-                CompoundValue::Compound(compound) => compound.resolve_name(scope_table)?,
+                for arg in &mut fn_call.args {
+                    arg.visit(self)?;
+                }
             }
-        }
-        Ok(())
-    }
-}
-
-impl Resolvable for FnCall {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        match scope_table.scope_lookup(&self.name.raw, SymbolType::Fn) {
-            Some(name) => {
-                self.name.module_resolved = Some(name.clone());
+            ASTNodeEnum::Block(block) => {
+                self.scope_enter();
+                for definitions in &mut block.definitions {
+                    definitions.visit(self)?;
+                }
+                for statement in &mut block.statements {
+                    statement.visit(self)?;
+                }
+                self.scope_exit();
             }
-            None => return Err(ResolverError::UndefinedVariable(self.name.raw.clone())),
-        }
+            ASTNodeEnum::Module(module) => {
+                self.scope_enter();
+                for use_ in &mut module.uses {
+                    use_.visit(self)?;
+                }
 
-        for arg in &mut self.args {
-            arg.resolve_name(scope_table)?;
-        }
-        Ok(())
-    }
-}
+                for definitions in &mut module.public_definitions {
+                    definitions.visit(self)?;
+                }
 
-impl Resolvable for Else {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        match self {
-            Else::If(if_) => if_.resolve_name(scope_table)?,
-            Else::Block(block) => block.resolve_name(scope_table)?,
-        }
-        Ok(())
-    }
-}
-
-impl Resolvable for If {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        self.cond.resolve_name(scope_table)?;
-        self.body.resolve_name(scope_table)?;
-        if let Some(ref mut else_body) = self.else_ {
-            else_body.resolve_name(scope_table)?;
-        }
-        Ok(())
-    }
-}
-
-impl Resolvable for While {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        self.cond.resolve_name(scope_table)?;
-        self.body.resolve_name(scope_table)?;
-        Ok(())
-    }
-}
-
-impl Resolvable for For {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        if let Some(ref mut init) = self.init {
-            init.resolve_name(scope_table)?;
-        }
-        if let Some(ref mut cond) = self.cond {
-            cond.resolve_name(scope_table)?;
-        }
-        if let Some(ref mut step) = self.step {
-            step.resolve_name(scope_table)?;
-        }
-        self.body.resolve_name(scope_table)?;
-        Ok(())
-    }
-}
-
-impl Resolvable for Use {
-    fn resolve_name(&mut self, scope_table: &mut ScopeTable) -> ResolveResult<()> {
-        for element in &mut self.elements {
-            let struct_name =
-                scope_table.scope_bind(&element.imported_name.raw, SymbolType::Struct)?;
-            let fn_name = scope_table.scope_bind(&element.imported_name.raw, SymbolType::Fn)?;
-            let var_name = scope_table.scope_bind(&element.imported_name.raw, SymbolType::Var)?;
-
-            if struct_name == fn_name && fn_name == var_name {
-                element.imported_name.module_resolved = Some(struct_name);
-            } else {
-                return Err(ResolverError::Redefinition(
-                    element.imported_name.raw.clone(),
-                ));
+                module.block.visit(self)?;
+                self.scope_exit();
             }
-        }
+            ASTNodeEnum::StructDef(struct_def) => {
+                struct_def.type_name.module_resolved =
+                    Some(self.scope_bind(&struct_def.type_name.raw, SymbolType::Struct)?);
 
-        Ok(())
+                for v in &mut struct_def.map.values_mut() {
+                    if let Type::Struct(struct_name) = v {
+                        struct_name.module_resolved =
+                            Some(self.scope_lookup_force(&struct_name.raw, SymbolType::Struct));
+                    }
+                }
+            }
+            ASTNodeEnum::Use(use_) => {
+                for element in &mut use_.elements {
+                    let struct_name =
+                        self.scope_bind(&element.imported_name.raw, SymbolType::Struct)?;
+                    let fn_name = self.scope_bind(&element.imported_name.raw, SymbolType::Fn)?;
+                    let var_name = self.scope_bind(&element.imported_name.raw, SymbolType::Var)?;
+
+                    if struct_name == fn_name && fn_name == var_name {
+                        element.imported_name.module_resolved = Some(struct_name);
+                    } else {
+                        return Err(ResolverError::Redefinition(
+                            element.imported_name.raw.clone(),
+                        ));
+                    }
+                }
+            }
+            _ => { return Ok(true); }
+        };
+        return Ok(false);
     }
 }
